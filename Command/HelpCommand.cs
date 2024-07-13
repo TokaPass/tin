ed592a5189cd76041d22201647d321f62fd7c72e
emirsassan <endwse@gmail.com>
2024-07-13 08:58:05 +0300

    init

---
+++ Command/HelpCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Tin.Command;
+
+[Command(Name = "help")]
+internal class ExampleCommand : ICommand
+{
+    public override void Exec(string[] strParams)
+    {
+        foreach (var param in strParams)
+        {
+            Console.WriteLine(param);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+namespace Tin;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var assembly = Assembly.GetEntryAssembly()
+            .GetTypes()
+            .Where(type => type.IsSubclassOf(typeof(ICommand))).ToList();
+
+        foreach (var type in assembly)
+        {
+            string cmdName = args[0];
+
+            foreach (var attribute in type.GetCustomAttributes(false))
+            {
+                CommandAttribute? attr = attribute as CommandAttribute;
+
+                if (attr != null)
+                {
+                    if (cmdName == attr.Name)
+                    {
+                        ICommand? command = Activator.CreateInstance(type) as ICommand;
+
+                        if (command != null)
+                        {
+                            command.Exec(args);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+public abstract class ICommand
+{
+    public virtual void Exec(string[] strParams)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class CommandAttribute : Attribute
+{
+    public string Name { get; set; }
+}
