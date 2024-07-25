9e1e5221071e5d87bde3691187712485c969638f
sassan <125366201+emirsassan@users.noreply.github.com>
2024-07-25 09:20:55 +0300

    changes to git tests

--- tests/git.test.ts
+++ tests/git.test.ts
@@ -3,11 +3,8 @@ import { $ } from "bun";
 import { cloneRepo } from "../src/helpers/git";
 
 describe("Git helper works as expected", () => {
-  beforeEach(async () => {
-    await $`rm -rf ./toka`;
-  });
   afterEach(async () => {
-    await $`rm -rf ./toka`;
+    await $`rm -rf toka`;
   });
   it("Can clone repos", async () => {
     let status = await cloneRepo("https://github.com/TokaPass/.github.git");
